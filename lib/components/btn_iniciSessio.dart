a2f678e86c979a687aad5e75cb37e542e85e1a13
nicolau.reyes <nicolau.reyes@mataro.epiaedu.cat>
2024-02-15 23:39:01 +0100

    Edit Button (A medias)

--- lib/components/btn_iniciSessio.dart
+++ lib/components/btn_iniciSessio.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({super.key});
+  final Function()? onTap;
+
+  const MyButton({Key? key, required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(25),
-      margin: const EdgeInsets.symmetric(horizontal: 25),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(8)
-      ),
-      child: const Center(
-        child: Text(
-          "Incia Sessió",
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+    return GestureDetector(
+      onTap: onTap,
+      child: SizedBox(
+        width: double.infinity, // Ancho máximo igual al ancho de la pantalla
+        child: Container(
+          padding: const EdgeInsets.all(25),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(8),
           ),
-        )
+          child: const Center(
+            child: Text(
+              "Iniciar Sesión",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
-} 
+}
