3b3879dfc317a53c46da211369f787c05f56e517
mohervictor <victorcrackball@gmail.com>
2024-03-21 17:19:33 +0100

    dades_familiars

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:kidneyproject/pages/dades_mediques.dart';
-//import 'package:kidneyproject/pages/login_page.dart'; 
-
 import 'package:kidneyproject/pages/menu_principal.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
