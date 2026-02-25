56fe037df888d023d2b7c7a240889ebe89dd81a7
geri <geripibo2004@gmail.com>
2026-02-25 20:53:31 +0100

    mevesDietesFam

--- lib/pages/menuDietes.dart
+++ lib/pages/menuDietes.dart
@@ -1,4 +1,4 @@
-import 'dart:async'; // Import necesario para Timer
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:kidneyproject/pages/crearDietes.dart';
@@ -9,8 +9,10 @@ import 'cestaProvider.dart';
 
 class MenuDietes extends StatefulWidget {
   final String userId;
+  final String pacienteId; // Id del paciente que ve el familiar (opcional)
 
-  const MenuDietes({Key? key, required this.userId}) : super(key: key);
+  const MenuDietes({Key? key, required this.userId, this.pacienteId = ''})
+      : super(key: key);
 
   @override
   _MenuDietesState createState() => _MenuDietesState();
@@ -20,7 +22,7 @@ class _MenuDietesState extends State<MenuDietes> {
   String _tipusC = '';
   String _tipoUsuario = '';
   bool _datosCargados = false;
-  bool _mostrarLoading = false; // Nuevo flag para mostrar loading
+  bool _mostrarLoading = false;
 
   @override
   void initState() {
@@ -28,9 +30,9 @@ class _MenuDietesState extends State<MenuDietes> {
     _startLoadingTimer();
     _loadData();
   }
-// funció per començar el temporitzador de càrrega
+
   void _startLoadingTimer() {
-    Timer(Duration(milliseconds: 500), () {
+    Timer(const Duration(milliseconds: 500), () {
       if (!_datosCargados) {
         setState(() {
           _mostrarLoading = true;
@@ -38,7 +40,7 @@ class _MenuDietesState extends State<MenuDietes> {
       }
     });
   }
-// funció per carregar les dades
+
   Future<void> _loadData() async {
     await _getTipusC();
     await _getTipoUsuario();
@@ -47,7 +49,7 @@ class _MenuDietesState extends State<MenuDietes> {
       _mostrarLoading = false;
     });
   }
-// Funció per obtenir el tipus C
+
   Future<void> _getTipusC() async {
     try {
       DocumentSnapshot docSnapshot = await FirebaseFirestore.instance
@@ -57,16 +59,13 @@ class _MenuDietesState extends State<MenuDietes> {
           .doc('datos')
           .get();
 
-      if (docSnapshot.exists) {
-        _tipusC = docSnapshot['tipusC'] ?? '';
-      } else {
-        _tipusC = 'Desconocido';
-      }
+      _tipusC =
+          docSnapshot.exists ? docSnapshot['tipusC'] ?? '' : 'Desconocido';
     } catch (e) {
       print('Error al obtener dades: $e');
     }
   }
-// funció per obtenir el tipus de l'usuari
+
   Future<void> _getTipoUsuario() async {
     try {
       DocumentSnapshot userDoc = await FirebaseFirestore.instance
@@ -76,9 +75,7 @@ class _MenuDietesState extends State<MenuDietes> {
           .doc('tipus')
           .get();
 
-      if (userDoc.exists) {
-        _tipoUsuario = userDoc['tipo'] ?? '';
-      }
+      if (userDoc.exists) _tipoUsuario = userDoc['tipo'] ?? '';
     } catch (e) {
       print('Error al obtener el tipo de usuario: $e');
     }
@@ -86,7 +83,6 @@ class _MenuDietesState extends State<MenuDietes> {
 
   @override
   Widget build(BuildContext context) {
-    // Accede al CestaProvider
     final cestaProvider = Provider.of<CestaProvider>(context, listen: false);
 
     return Scaffold(
@@ -94,40 +90,36 @@ class _MenuDietesState extends State<MenuDietes> {
         title: const Text('Menú Dietes'),
         backgroundColor: Colors.greenAccent,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            // Comprobamos si hay elementos en la cesta
-            if (cestaProvider.cestaItems.isNotEmpty) { // Usa cestaProvider aquí
-              // Si la cesta tiene elementos, mostramos un dialog de confirmación
+            if (cestaProvider.cestaItems.isNotEmpty) {
               showDialog(
                 context: context,
                 builder: (context) => AlertDialog(
-                  title: Text('Alerta'),
-                  content: Text('Tens elements en la cistella. Segur que et vols dirigir al menú principal?'),
+                  title: const Text('Alerta'),
+                  content: const Text(
+                      'Tens elements en la cistella. Segur que et vols dirigir al menú principal?'),
                   actions: [
                     TextButton(
-                      onPressed: () {
-                        Navigator.pop(context); // Cerrar el dialog
-                      },
-                      child: Text('Cancelar'),
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text('Cancelar'),
                     ),
                     ElevatedButton(
                       onPressed: () {
-                        Navigator.pop(context); // Cerrar el dialog
+                        Navigator.pop(context);
                         Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => MenuPrincipal(userId: widget.userId),
-                          ),
+                              builder: (context) =>
+                                  MenuPrincipal(userId: widget.userId)),
                         );
                       },
-                      child: Text('Sí, vull continuar'),
+                      child: const Text('Sí, vull continuar'),
                     ),
                   ],
                 ),
               );
             } else {
-              // Si no hay elementos en la cesta, simplemente navegar
               Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(
@@ -143,61 +135,87 @@ class _MenuDietesState extends State<MenuDietes> {
           padding: const EdgeInsets.all(20.0),
           child: _datosCargados
               ? Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-              if (_tipoUsuario != 'Familiar')
-                ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => crearDietes(userId: widget.userId, tipusC: _tipusC),
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 50),
+
+                    // Crear dietes
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => crearDietes(
+                                userId: widget.userId, tipusC: _tipusC),
+                          ),
+                        );
+                      },
+                      icon:
+                          const Icon(Icons.add, size: 30, color: Colors.white),
+                      label: const Text('Crear Dietes',
+                          style: TextStyle(fontSize: 25)),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.greenAccent,
+                        minimumSize: const Size(300, 70),
                       ),
-                    );
-                  },
-                  icon: const Icon(
-                    Icons.add,
-                    size: 30,
-                    color: Colors.white,
-                  ),
-                  label: const Text(
-                    'Crear Dietes',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.greenAccent,
-                    minimumSize: Size(300, 70),
-                  ),
-                ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => lesMevesDietes(userId: widget.userId, tipusC: _tipusC),
                     ),
-                  );
-                },
-                child: const Text(
-                  'Veure Dietes',
-                  style: TextStyle(fontSize: 25),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
-                  minimumSize: Size(300, 70),
-                ),
-              ),
-            ],
-          )
+                    const SizedBox(height: 30),
+
+                    // Ver dietes del paciente (si es familiar)
+                    ElevatedButton(
+                      onPressed: () {
+                        final idParaVer = _tipoUsuario == 'Familiar' &&
+                                widget.pacienteId.isNotEmpty
+                            ? widget.pacienteId
+                            : widget.userId;
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => lesMevesDietes(
+                                userId: idParaVer,
+                                tipusC: _tipusC,
+                                mostrarSoloPropias: false),
+                          ),
+                        );
+                      },
+                      child: const Text('Veure Dietes',
+                          style: TextStyle(fontSize: 25)),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.greenAccent,
+                        minimumSize: const Size(300, 70),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+
+                    // Ver dietes propias del familiar
+                    if (_tipoUsuario == 'Familiar')
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => lesMevesDietes(
+                                  userId: widget.userId,
+                                  tipusC: _tipusC,
+                                  mostrarSoloPropias: true),
+                            ),
+                          );
+                        },
+                        child: const Text('Veure les meves dietes creades',
+                            style: TextStyle(fontSize: 25)),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orangeAccent,
+                          minimumSize: const Size(300, 70),
+                        ),
+                      ),
+                  ],
+                )
               : _mostrarLoading
-              ? CircularProgressIndicator() // Solo se muestra si tarda más de 500ms
-              : Container(), // No muestra nada si aún no pasó el tiempo
+                  ? const CircularProgressIndicator()
+                  : Container(),
         ),
       ),
     );
   }
-
-
 }
